bd3c54c7088988150b93d7f34af06a67f764759f
Facundo Nogueira <97248784+FNogueira2023@users.noreply.github.com>
2024-01-25 12:32:36 -0500

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
-def call(String imageName){
+def call(String image_name){
         echo 'building the image...'
         withCredentials([usernamePassword(credentialsId:'docker_hub', passwordVariable: 'PASSWORD', usernameVariable: 'USER' )]) {
-            sh "docker build -t $imageName ."
+            sh "docker build -t $image_name ."
             sh "echo $PASSWORD | docker login -u $USER --password-stdin"
-            sh "docker push $imageName"
+            sh "docker push $image_name"
                 
     }
 }
