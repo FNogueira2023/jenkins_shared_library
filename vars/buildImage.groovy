c496eea5f94a55e028340b459630cd073b3b6221
Facundo Nogueira <97248784+FNogueira2023@users.noreply.github.com>
2024-01-25 12:01:49 -0500

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 def call(String imageName){
         echo 'building the image...'
         withCredentials([usernamePassword(credentialsId:'docker_hub', passwordVariable: 'PASSWORD', usernameVariable: 'USER' )]) {
-            sh "docker build -t $imageName ."
+            sh "docker build -t fnogueira2023/multi_peru_5:1.0 ."
             sh "echo $PASSWORD | docker login -u $USER --password-stdin"
-            sh "docker push $imageName "
+            sh "docker push fnogueira2023/multi_peru_5:1.0"
 
     }
 }
